9611673540f452876509c42f86d88d55b198d480
Pablo Chico de Guzman <pchico83@gmail.com>
2020-09-14 07:40:48 +0200

    Init version

---
+++ README.md
@@ -0,0 +1,11 @@
+<p align="center">
+<a href="https://cloud.okteto.com/deploy">
+  <img src="https://okteto.com/develop-okteto.svg" alt="Develop on Okteto">
+</a>
+</p>
+
+## Tutorial
+
+![App Screenshot](screenshot.png)
+
+TBD
---
+++ api/src/main/java/com/example/polls/controller/PollController.java
@@ -0,0 +1,79 @@
+package com.example.polls.controller;
+
+import com.example.polls.model.*;
+import com.example.polls.payload.*;
+import com.example.polls.repository.PollRepository;
+import com.example.polls.repository.UserRepository;
+import com.example.polls.repository.VoteRepository;
+import com.example.polls.security.CurrentUser;
+import com.example.polls.security.UserPrincipal;
+import com.example.polls.service.PollService;
+import com.example.polls.util.AppConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import javax.validation.Valid;
+import java.net.URI;
+
+/**
+ * Created by rajeevkumarsingh on 20/11/17.
+ */
+
+@RestController
+@RequestMapping("/api/polls")
+public class PollController {
+
+    @Autowired
+    private PollRepository pollRepository;
+
+    @Autowired
+    private VoteRepository voteRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PollService pollService;
+
+    private static final Logger logger = LoggerFactory.getLogger(PollController.class);
+
+    @GetMapping
+    public PagedResponse<PollResponse> getPolls(@CurrentUser UserPrincipal currentUser,
+                                                @RequestParam(value = "page", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
+                                                @RequestParam(value = "size", defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size) {
+        return pollService.getAllPolls(currentUser, page, size);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> createPoll(@Valid @RequestBody PollRequest pollRequest) {
+        Poll poll = pollService.createPoll(pollRequest);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{pollId}")
+                .buildAndExpand(poll.getId()).toUri();
+
+        return ResponseEntity.created(location)
+                .body(new ApiResponse(true, "Poll Created Successfully"));
+    }
+
+
+    @GetMapping("/{pollId}")
+    public PollResponse getPollById(@CurrentUser UserPrincipal currentUser,
+                                    @PathVariable Long pollId) {
+        return pollService.getPollById(pollId, currentUser);
+    }
+
+    @PostMapping("/{pollId}/votes")
+    @PreAuthorize("hasRole('USER')")
+    public PollResponse castVote(@CurrentUser UserPrincipal currentUser,
+                         @PathVariable Long pollId,
+                         @Valid @RequestBody VoteRequest voteRequest) {
+        return pollService.castVoteAndGetUpdatedPoll(pollId, voteRequest, currentUser);
+    }
+
+}
